d654ac3af2310545a97b5b37b23a0d78aaadc25a
m4cit <92433046+m4cit@users.noreply.github.com>
2022-07-25 13:26:37 +0200

    Delete slangPD directory

--- slangPD/misc/information.py
+++
@@ -1,9 +0,0 @@
-def info():
-    print(' Please pass "train" first to train the models:\n predict_main.py train\n\n Then a number for whichever classifier you want to use, and the sentence you want to evaluate, like this:\n predict_main.py number "sentence"\n\n',
-    '1 for Linear SVM\n 2 for Decision Tree\n 3 for Naive Bayes (Gaussian)\n 4 for Naive Bayes (Multinomial)\n 5 for Logistic Regression\n 6 for all classifiers\n')
-
-def info2():
-    print(' You can also type -info to display your input options again.\n')
-
-def in_opt():
-    print('\n 1 for Linear SVM\n 2 for Decision Tree\n 3 for Naive Bayes (Gaussian)\n 4 for Naive Bayes (Multinomial)\n 5 for Logistic Regression\n 6 for all classifiers\n')
